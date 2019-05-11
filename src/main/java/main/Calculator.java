4baebf6809f042e153f8d82d68d548ba5c56b136
yogesh kumar <yogesh kumar@MSI>
2019-05-11 21:58:24 +0530

    written the calculator function successfully and obtained the results

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -13,7 +13,9 @@ public class Calculator {
         System.out.println("Enter your selection: 1 for Addition, 2 for substraction 3 for Multiplication and 4 for division:");
         int choose;
         choose = inp.nextInt();
-        switch (choose){
+        switch (choose)
+        {
+        
         case 1:
             System.out.println(add( num1,num2));
             break;
@@ -38,26 +40,25 @@ public class Calculator {
     public static int add(int x, int y)
     {
     	int result=0;
-//        write your code here 
-    	
+       result = x + y; 
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
-//        write your code here
+    result = x - y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+        result = x * y;     
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
-//      write your code here
+     result = x/y;
     	return result;
     }
 
